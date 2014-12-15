147b30c9de0c8bbc48f15c9c003b9becbc249136
hitode909 <hitode909@gmail.com>
2014-12-15 15:10:58 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Middleware::Bootstrap - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::Bootstrap;
+
+# DESCRIPTION
+
+Plack::Middleware::Bootstrap is ...
+
+# LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hitode909 <hitode909@gmail.com>
---
+++ lib/Plack/Middleware/Bootstrap.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Bootstrap;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Bootstrap - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::Bootstrap;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Bootstrap is ...
+
+=head1 LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hitode909 E<lt>hitode909@gmail.comE<gt>
+
+=cut
+
